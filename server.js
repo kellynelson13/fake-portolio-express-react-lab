80466abf529be841543bd66e48969c6a13a24259
Kelly Nelson <kelnel86@gmail.com>
2022-09-27 15:52:13 -0500

    got test routes going on server.js

--- server.js
+++ server.js
@@ -4,3 +4,29 @@ const projects = require("./projects.json");
 const about = require("./about.json");
 
 const app = express();
+
+//set up middleware
+app.use(cors());
+
+//home route for testing our app
+app.get("/", (req, res) => {
+    res.send("hello portfolio")
+})
+
+//route for retrieving projects
+app.get("/projects", (req, res) => {
+    // send projects via JSON
+    res.json(projects);
+})
+
+//route for retrieving about info
+app.get("/about", (req, res) => {
+    // send about info via JSON
+    res.json(about);
+})
+
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+
+//turn on the server listener
+app.listen(PORT, console.log(`Listening at port ${PORT}`))
